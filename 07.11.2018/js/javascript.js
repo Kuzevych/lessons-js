b849a06181f8c701fd418a423ee78e99ede07d9a
Mykola Kuzevych <m.kuzevych@gmail.com>
2018-11-07 15:39:32 +0200

    change if to switch

--- 07.11.2018/js/javascript.js
+++ 07.11.2018/js/javascript.js
@@ -5,17 +5,25 @@
     let divide = (a,b) => Number(a) / Number(b);
     let  multiply = (a,b) => Number(a) * Number(b);
 
-document.getElementById('start').onclick = function() {
-    let answer = prompt('Select function: add or +, substract or -, divide or /, multiply or *','add');
+function start() {
+    let answer = prompt('Select function to: add "+", substract "-", divide "/", multiply "*"','add');
     let a = prompt('Write first number',1);
-    let b =prompt('Write second number',2);
-    if (answer==='add' || answer==='+'){
-       document.getElementById('result').innerHTML = add(a,b).toFixed(1);
-    } else if (answer==='subtract' || answer==='-') {
-        document.getElementById('result').innerHTML = subtract(a,b).toFixed(1);
-    } else if (answer==='divide' || answer==='/') {
-        document.getElementById('result').innerHTML = divide(a,b).toFixed(1);
-    } else if (answer==='multiply' || answer==='*') {
-        document.getElementById('result').innerHTML = multiply(a,b).toFixed(1);
+    let b = prompt('Write second number',2);
+    
+    switch (answer) {
+        case '+':
+            document.getElementById('result').createTextNode(add(a,b).toFixed(1));
+            break;
+        case '-':
+            document.getElementById('result').innerHTML = subtract(a,b).toFixed(1);
+            break;
+        case '/':
+            document.getElementById('result').innerHTML = divide(a,b).toFixed(1);
+            break;
+        case '*':
+            document.getElementById('result').innerHTML = multiply(a,b).toFixed(1);
+            break;
     }
 };
+
+
