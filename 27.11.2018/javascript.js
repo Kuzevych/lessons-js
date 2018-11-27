d5501a2fe34c0859a77aaf0aeb44279600785cf4
Mykola Kuzevych <m.kuzevych@gmail.com>
2018-11-27 17:16:19 +0200

    object add new function

--- 27.11.2018/javascript.js
+++ 27.11.2018/javascript.js
@@ -1,16 +1,29 @@
+
 function Student(){
 
 }
 
 
-var student1 = new Student ({name: 'Ivan',
+var student1 = {
+    name: 'Ivan',
     surname: 'Ivanov',
-    grades: [4,5,5]});
-var student2 = new Student ({name: 'Semen',
+    age: 22,
+    grades: [4, 5, 5],
+    getAvgGrave: function () {
+        var avg = this.grades.reduce(function (sum, cur) {
+            return sum + cur;
+        })
+        return (avg / this.grades.length).toFixed(2);
+    }
+}
+var student2 = {name: 'Semen',
     surname: 'Semenov',
-    grades: [3,5,5]});
-var student3 = new Student ({name: 'Petr',
+    grades: [3,5,5]
+};
+var student3 = {name: 'Petr',
     surname: 'Petrov',
-    grades: [4,3,5]});
-console.log(student3);
+    grades: [4,3,5]
+};
+
+console.log(student1.getAvgGrave());
 
