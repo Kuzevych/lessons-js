2ceeee30540a5f03410b0b14d3c84c8f785a8e95
Kolya Kuzevych <m.kuzevych@gmail.com>
2018-12-06 21:50:25 +0200

    less 06.12 all tasks check

--- 06.12.2018 practice/javascript.js
+++ 06.12.2018 practice/javascript.js
@@ -70,3 +70,74 @@
 // }
 // getField(test, 'firstName');
 
+
+// function initCounter() {
+//     var count = 1;
+//     return function (){
+//         return count++;
+//     }
+// }
+//
+// var counter = initCounter();
+// var counter2 = initCounter();
+//
+// alert(counter());
+// alert(counter());
+// alert(counter());
+// alert(counter());
+
+
+// function makeCounter (first, step){
+//     var count = first;
+//     return function(){
+//         return count+=step;
+//     }
+// }
+//
+// var counter = makeCounter(0,2);
+//
+// function pick(myCount, step){
+//     var array = [];
+//     for (var i = 0; i<step; i++){
+//         array.push(counter());
+//     }
+//     return array;
+//     // console.log(array);
+// }
+//
+// console.log(pick(counter,5));
+
+
+// function square (x) {
+//     return x*x;
+// }
+//
+// function map(number,array) {
+//     var newArr = [];
+//     for (var i = 0; i < array.length ; i++) {
+//         newArr.push(square(array[i]));
+//     }
+//     return newArr;
+// }
+// console.log(map(square,[4,5,6,7,8,9]));
+// console.log(map(square, []));
+
+
+// var input = [1, 2, 3, 4, 5,]
+//
+// function isObb(x) {
+//     if (x%2===1){
+//         return x;
+//     }
+// }
+// function filter(array, func){
+//     var newArr =[];
+//     for (var i = 0; i < array.length; i++) {
+//         if (func(array[i])){
+//             newArr.push(array[i]);
+//         }
+//     }
+//     return newArr;
+// }
+// console.log(filter(input, isObb));
+
