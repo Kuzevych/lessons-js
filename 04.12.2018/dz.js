69aa7be9e46691b5e6cb4f557800ccdda02b2c0b
Mykola Kuzevych <m.kuzevych@gmail.com>
2018-12-10 09:34:07 +0200

    few changes

--- 04.12.2018/dz.js
+++ 04.12.2018/dz.js
@@ -1,6 +1,8 @@
 
+Student.group = [];
+Student.showAllStudents = [];
 
-function Student (name, surname, grades) {
+function Student(name, surname, grades) {
     this.name = name,
         this.surname = surname,
         this.grades = grades,
@@ -10,37 +12,22 @@ function Student (name, surname, grades) {
             grades: grades
         }),
         this.fullName = function () {
-            return(this.name + ' ' + this.surname);
+            return (this.name + ' ' + this.surname);
         },
-        this.gradesAvarage = function(){
+        this.gradesAverage = function () {
             var avg = this.grades.reduce(function (sum, cur) {
                 return sum + cur;
             });
-            return ('Середній бал - ' + this.name + ' ' + this.surname + ' '+ (avg / this.grades.length).toFixed(2));
+            return (avg / this.grades.length).toFixed(2);
         }
-}
-Student.group = [];
-
-Student.getBestStudents = function () {
-    Student.listStudents.filter(
-        function (item) {
-            return item.mark > 90;
-        }
-    ).forEach(function(item) {
-        console.log(item.name, item.mark)
-    })
 };
 
-var student1 = new Student ('Ivan', 'Ivanov', [4,5,5]);
-var student2 = new Student ('Semen', 'Semenov', [3,5,5]);
-var student3 = new Student ('Petr', 'Petrov', [4,3,5]);
-
+var student1 = new Student('Ivan', 'Ivanov', [4, 5, 5]);
+var student2 = new Student('Semen', 'Semenov', [3, 5, 5]);
+var student3 = new Student('Petr', 'Petrov', [4, 3, 5]);
+var student4 = new Student('Kolya', 'Kuzevych', [5, 5, 5]);
 
 console.log(student1.fullName());
-console.log(student1.gradesAvarage());
-console.log(student2.fullName());
-console.log(student2.gradesAvarage());
-console.log(student3.fullName());
-console.log(student3.gradesAvarage());
+console.log(student1.gradesAverage());
 
-//console.log(Student.group);
+console.log(Student.group);
