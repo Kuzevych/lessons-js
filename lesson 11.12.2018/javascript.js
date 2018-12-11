75c360a03e1516dd5d1c9c7d8f7517b19a9647cd
Kolya Kuzevych <m.kuzevych@gmail.com>
2018-12-11 19:37:03 +0200

    less 11.12 start

---
+++ lesson 11.12.2018/javascript.js
@@ -0,0 +1,27 @@
+// function logArray(params1){
+//     console.log(this);
+//     console.log(params1);
+// }
+//
+// var arr = [1,2,3];
+// logArray.call(arr, 'test');
+
+
+// var obj = {
+//     testKey: 'test'
+// };
+// function foo() {
+//     console.log (this.testKey);
+// }
+// foo.call(obj);
+
+
+// var obj = {
+//     testKey: 'test',
+//     secKey: 'second',
+//     thirdKey: 'third'
+// };
+// function foo(key){
+//     console.log(this[key]);
+// }
+// foo.call(obj, 'testKey');
