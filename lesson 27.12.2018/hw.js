b10c5f7a8159fa11d6cec7a90218f573ccfd43b7
Mykola Kuzevych <m.kuzevych@gmail.com>
2019-01-09 12:02:15 +0200

    hw from 27.12.2018 first part - done

--- lesson 27.12.2018/hw.js
+++ lesson 27.12.2018/hw.js
@@ -2,25 +2,25 @@ var div = document.querySelector('div');
 
 function hasClass(node,klass){
         if (node.classList.contains(klass)){
-            console.log('node' + ' has class ' + klass);
+            console.log('YES');
             return true;
         } else {
-            console.log('false');
+            console.log('NO');
+            return false;
         }
 }
-hasClass(div,'third');
 
 function addClass(node,klass) {
     node.classList.add(klass);
     console.log(node);
 }
-addClass(div,'third');
 
 function removeClass(node,klass){
-    if (hasClass(node,klass)){
-        node.classList.remove(klass);
-    }
+    node.classList.toggle(klass);
     console.log(node);
 }
 
-removeClass(div, 'cl');
+
+hasClass(div,'fisst');
+addClass(div,'newClass');
+removeClass(div, 'first');
