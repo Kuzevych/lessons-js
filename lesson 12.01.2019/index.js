13181de42abeb4c16f34c3d454f09246a3d548d5
Mykola Kuzevych <m.kuzevych@gmail.com>
2019-01-14 11:08:38 +0200

    start home work with events js

--- lesson 12.01.2019/index.js
+++ lesson 12.01.2019/index.js
@@ -1,22 +1,36 @@
+//
+// var options = {
+//   top: 24,
+//   right: 50,
+//   color: 'red',
+//   className: 'notification',
+//   html: 'text'
+// };
+//
+// function showNotification(options){
+//     var element = document.createElement('div');
+//     element.textContent = options.html;
+//     element.className = options.className;
+//     element.style.marginTop = options.top + 'px';
+//     element.style.marginRight = options.right + 'px';
+//     element.style.color = options.color;
+//      setTimeout(function(){
+//          document.body.appendChild(element);
+//      },2000)
+//
+// }
+// showNotification(options);
 
-var options = {
-  top: 24,
-  right: 50,
-  color: 'red',
-  className: 'notification',
-  html: 'text'
+
+window.onload = function() {
+    for (var i = 0; i < 10; i++) {
+        var button = document.createElement('button');
+        // button.createTextNode('click');
+        document.body.appendChild(button);
+        button.onclick = function () {
+            alert(i);
+        }
+
+    }
 };
 
-function showNotification(options){
-    var element = document.createElement('div');
-    element.textContent = options.html;
-    element.className = options.className;
-    element.style.marginTop = options.top + 'px';
-    element.style.marginRight = options.right + 'px';
-    element.style.color = options.color;
-     setTimeout(function(){
-         document.body.appendChild(element);
-     },2000)
-
-}
-showNotification(options);
