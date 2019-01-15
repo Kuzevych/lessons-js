eca5e625bee9a5b912d2bc7d1c08eb1e5e7e320e
Mykola Kuzevych <m.kuzevych@gmail.com>
2019-01-15 20:57:40 +0200

    start lesson 15.01.19: part 2 start

--- lesson 15.01.2019 events/index.js
+++ lesson 15.01.2019 events/index.js
@@ -1,6 +1,7 @@
 
 var parent = document.getElementById('parent');
 var childs = document.getElementsByClassName('child');
+var contents = document.querySelectorAll('#content>p');
 
 function handler(){
     event.stopPropagation();
@@ -13,8 +14,25 @@ function handler(){
 
 //parent.onclick = handler.bind(parent,'test');
 
-parent.onclick = handler;
+//parent.onclick = handler;
+//
+// [].forEach.call(childs,function (child) {
+//     child.onclick = handler;
+// });
 
-[].forEach.call(childs,function (child) {
-    child.onclick = handler;
-});
+// parent.addEventListener('click',function () {
+//     var chekedElements = event.target;
+//     if (!chekedElements.hasAttribute('data-content-id')) return;
+//     var contentId = chekedElements.getAttribute('data-content-id');
+//     [].forEach.call(contents, function (content) {
+//         if(content.id === contentId) {
+//             content.hidden = false;
+//         } else {
+//             content.hidden = true;
+//         }
+//         //те ж саме, що нижче
+//
+//         //content.hidden = content.id !== contentId;
+//     })
+// });
+
