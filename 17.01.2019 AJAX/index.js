9f0152ac25cf732fb72dcfebeb0ac3640e7ce792
Mykola Kuzevych <m.kuzevych@gmail.com>
2019-01-17 21:53:15 +0200

    second part of lesson (AJAX topic)

--- 17.01.2019 AJAX/index.js
+++ 17.01.2019 AJAX/index.js
@@ -15,10 +15,9 @@
 
 
 //Promise request + fetch!!
-
+setInterval(
 var button = document.getElementById('get');
-
-function start(){
+function start() {
     // var promise = new Promise(function(resolve,reject){
     //     var xhttp = new XMLHttpRequest();
     //     xhttp.open('GET', 'https://api.exchangeratesapi.io/latest',true);
@@ -31,20 +30,45 @@ function start(){
     //         }
     //     };
     // });
-
-    var promise = fetch('https://api.exchangeratesapi.io/latest',{
-        method:'GET'
-    });
-
-    promise
-        .then(function (response) {
-        //console.log(data);
+    var options = {
+        method: 'GET'
+    };
+    var promise = fetch('https://api.exchangeratesapi.io/latest', options).then(function (response) {
         return response.json();
-    })
+    });
+    var promise2 = fetch('https://api.exchangeratesapi.io/latest?symbols=USD,GBP', options).then(function (response) {
+        return response.json();
+    });
+    var promise3 = fetch('https://api.exchangeratesapi.io/history?start_at=2018-01-01&end_at=2018-09-01', options).then(function (response) {
+        return response.json();
+    });
+    Promise.all([
+        promise, promise2, promise3
+    ])
+        .then(function (response) {
+            return response;
+        })
         .then(function (data) {
             console.log(data);
         })
-        .catch(function(error){
-        console.log(error);
-    });
 }
+    // promise
+    //     .then(function (response) {
+    //         return response.json();
+    //     })
+    //     .then (function(data){
+    //         console.log(data);
+    //         if(data.base === 'EUR'){
+    //             return promise2;
+    //         }
+    //     })
+    //     .then(function (promise2Response) {
+    //     //console.log(data);
+    //     return promise2Response.json();
+    // })
+    //     .then(function (response2) {
+    //         console.log(response2);
+    //     })
+    //     .catch(function(error){
+    //     console.log(error);
+    // });
