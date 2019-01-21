115d563d2c22f45f05cc1e840d4d3751be0aa48b
Kolya Kuzevych <m.kuzevych@gmail.com>
2019-01-21 23:10:16 +0200

    AJAX hw (already find latest object)

--- 17.01.2019 AJAX/at home/index.js
+++ 17.01.2019 AJAX/at home/index.js
@@ -1,6 +1,5 @@
 var btn = document.getElementById('start');
 
-
 btn.onclick = function () {
     var option = {
         method:'GET'
@@ -8,15 +7,25 @@ btn.onclick = function () {
     var promise = fetch('https://api.exchangeratesapi.io/history?start_at=2019-01-07&end_at=2019-01-20&symbols=USD,RUB', option).then(function (response) {
         return response.json();
     });
-
-    promise
+     promise
         .then(function(response){
             return response
         })
         .then(function (data) {
-            console.log(data.rates);
+            for (var key in data) {
+                 if (typeof data[key] === 'object') {
+                     //console.log(data[key]);
+                     for (var i in data[key]){
+                         console.log(data[key][i]);
+                     }
+                 }
+            }
+            //console.log(data);
         });
 };
+
+
+
 
 
 
