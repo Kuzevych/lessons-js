e970df74b49a6067713b6e9dca187b130fcba52c
Mykola Kuzevych <m.kuzevych@gmail.com>
2019-01-22 16:27:05 +0200

    hw AJAX - almost done - http://prntscr.com/malho5

--- 17.01.2019 AJAX/at home/index.js
+++ 17.01.2019 AJAX/at home/index.js
@@ -1,10 +1,11 @@
 var btn = document.getElementById('start');
 
 btn.onclick = function () {
+
     var option = {
         method:'GET'
     };
-    var promise = fetch('https://api.exchangeratesapi.io/history?start_at=2019-01-07&end_at=2019-01-20&symbols=USD,RUB', option).then(function (response) {
+    var promise = fetch('https://api.exchangeratesapi.io/history?start_at=2019-01-06&end_at=2019-01-20&symbols=USD', option).then(function (response) {
         return response.json();
     });
      promise
@@ -12,18 +13,32 @@ btn.onclick = function () {
             return response
         })
         .then(function (data) {
-            for (var key in data) {
-                 if (typeof data[key] === 'object') {
-                     //console.log(data[key]);
-                     for (var i in data[key]){
-                         console.log(data[key][i]);
-                     }
-                 }
+            var dates = [];
+            var valuesUsd = [];
+            console.log(data);
+            for (var date in data.rates){
+                dates.push(date)
+                valuesUsd.push(data.rates[date].USD);
             }
-            //console.log(data);
+            console.log(dates);
+            console.log(valuesUsd);
+            var ctx = document.getElementById('myChart').getContext('2d');
+            var chart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: dates,
+                    datasets: [{
+                        label: "EUR to USD per last 14 days",
+                        backgroundColor: '#FD0A0A',
+                        borderColor: '#000000',
+                        data: valuesUsd,
+                    }]
+                },
+                options: {}
+            });
+
         });
 };
-
 
 
 
