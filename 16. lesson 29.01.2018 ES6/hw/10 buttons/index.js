ba27e1b5cd4fb7dd0958b051371caa2ad0341d6c
Mykola Kuzevych <m.kuzevych@gmail.com>
2019-02-04 11:04:46 +0200

    hw 31.01.2019...

--- 16. lesson 29.01.2018 ES6/hw/10 buttons/index.js
+++ 16. lesson 29.01.2018 ES6/hw/10 buttons/index.js
@@ -5,6 +5,7 @@ window.onload = () => {
         let btn = document.createElement('button');
         btn.textContent = i;
         document.body.appendChild(btn);
+        btn.style.marginRight = 20+'px';
         btn.onclick = () => alert(i);
     }
 };
