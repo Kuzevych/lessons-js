f9788924ef57aa4a764108d7145f99d5c6ec860b
Mykola Kuzevych <m.kuzevych@gmail.com>
2019-02-06 22:52:55 +0200

    hw 15.01.2019 - mini random calculator - almost finish

--- 12. lesson 15.01.2019 events/hw/javascript.js
+++ 12. lesson 15.01.2019 events/hw/javascript.js
@@ -1,34 +1,53 @@
-function getRandomNumber(min,max) {
+let getRandomNumber = (min,max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-const symbolArray = ['+','-','*','/'];
+};
 
-function startFunction() {
+const symbolArray = ['+','-','*','/'];
+const resultEquationArray = [];
+
+
+let equation = (firstNumber,secondNumber, symbol) => {
+    if (symbol === '+'){
+        return firstNumber + secondNumber;
+    } else if (symbol ==='-'){
+        return firstNumber - secondNumber;
+    } else if (symbol === '*'){
+        return firstNumber * secondNumber;
+    } else if (symbol === '/') {
+        return firstNumber / secondNumber;
+    }
+};
+
+let createEquation = () => {
     for (let i = 0; i < 10; i++) {
         const div = document.createElement('div');
-        div.className = 'class'+i;
+        div.className = 'equation';
         const p = document.createElement('p');
         const input = document.createElement('input');
+        const resultP = document.createElement('p');
+        resultP.className = 'result';
         const firstNumber = getRandomNumber(100, 1);
         const secondNumber = getRandomNumber(20, 1);
-        p.textContent = firstNumber + symbolArray[getRandomNumber(4, 0)] + secondNumber;
+        const randSymbol = symbolArray[getRandomNumber(4, 0)];
+        resultEquationArray.push(equation(firstNumber,secondNumber,randSymbol).toFixed(0));
+        p.textContent = firstNumber + randSymbol + secondNumber;
         document.body.appendChild(div);
         div.appendChild(p);
         div.appendChild(input);
+        div.appendChild(resultP);
     }
-}
+    return resultEquationArray;
+};
 
 
-function check(){
-    const divs = document.querySelectorAll('div');
-    for (let i = 1; i < 11; i++) {
-        const p = document.createElement('p');
-        const first = Number(divs[i].firstElementChild.textContent);
-        const second = Number(divs[i].children[1].value);
-        p.textContent = first + ''+ '' + second;
-        //divs.children[i].appendChild(p);
-        console.log(divs.children[i]);
-        //console.log(first);
-        //console.log(typeof second);
+let check = () => {
+    const divs = document.querySelectorAll('.equation');
+    for (let i = 0; i < divs.length; i++) {
+        if(resultEquationArray[i] === divs[i].children[1].value) {
+            divs[i].children[2].textContent = 'true';
+        } else {
+            divs[i].children[2].textContent = 'false';
+        }
+//        console.log(divs[i].children[2].textContent = 'true');
     }
-}
+};
---
+++ 12. lesson 15.01.2019 events/hw/settings.js
@@ -0,0 +1,10 @@
+let settings = () => {
+    const checkboxesBlock = document.querySelector('.checkboxes');
+    if (checkboxesBlock.hidden === true) {
+        checkboxesBlock.hidden = false;
+    } else {
+        checkboxesBlock.hidden = true;
+    }
+};
+
+
