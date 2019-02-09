95b9c2bef13f5de9dd1203056b89f3acfe0f99ea
Kolya Kuzevych <m.kuzevych@gmail.com>
2019-02-10 01:43:40 +0200

    Student class ES6: add few new functions

--- 16. lesson 29.01.2018 ES6/hw/Student ES6/javascript.js
+++ 16. lesson 29.01.2018 ES6/hw/Student ES6/javascript.js
@@ -7,9 +7,16 @@ class Student {
     fullName(){
         return `${this.name} ${this.surname}`;
     }
+d    gradesAverage(){
+        let avg = this.grades.reduce((sum, cur) => {
+            return sum + cur;
+        });
+        return (avg / this.grades.length).toFixed(2);
+    }
 }
 
-const student1 = new Student('Ko','Ku',[3,4,5]);
+const student1 = new Student('Ko','Ku',[5,5,5]);
 console.log(student1.fullName());
+console.log(student1.gradesAverage());
 const student2 = new Student('Semen', 'Semenov', [3, 5, 5]);
 const student3 = new Student('Petr', 'Petrov', [4, 3, 5]);
