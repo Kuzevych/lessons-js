507dd03cce1012ac34ca0e7700fe570fca894e6d
Kolya Kuzevych <m.kuzevych@gmail.com>
2019-02-16 21:57:42 +0200

    Student class ES6: work....

--- 16. lesson 29.01.2018 ES6/hw/Student ES6/javascript.js
+++ 16. lesson 29.01.2018 ES6/hw/Student ES6/javascript.js
@@ -7,11 +7,14 @@ class Student {
     fullName(){
         return `${this.name} ${this.surname}`;
     }
-d    gradesAverage(){
+    gradesAverage(){
         let avg = this.grades.reduce((sum, cur) => {
             return sum + cur;
         });
         return (avg / this.grades.length).toFixed(2);
+    }
+    _showName(){
+        return `Name is:${this.name}`;
     }
 }
 
@@ -20,3 +23,5 @@ console.log(student1.fullName());
 console.log(student1.gradesAverage());
 const student2 = new Student('Semen', 'Semenov', [3, 5, 5]);
 const student3 = new Student('Petr', 'Petrov', [4, 3, 5]);
+
+console.log(student1._showName());
